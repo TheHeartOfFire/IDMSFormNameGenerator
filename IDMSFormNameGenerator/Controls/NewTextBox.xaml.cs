999470c09a744b9363c79d690bc5415edda85d29
Dakota Jordan <Dakota.Jordan@Solera.com>
2022-05-18 12:35:43 -0500

    Add project files.

---
+++ IDMSFormNameGenerator/Controls/NewTextBox.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IDMSFormNameGenerator.Controls
+{
+    /// <summary>
+    /// Interaction logic for TextBox.xaml
+    /// </summary>
+    public partial class NewTextBox : UserControl
+    {
+        private static readonly DependencyProperty text = DependencyProperty.Register(
+            "Text", typeof(string),
+            typeof(NewTextBox),
+            new FrameworkPropertyMetadata(
+                defaultValue: string.Empty,
+            FrameworkPropertyMetadataOptions.AffectsRender,
+            new PropertyChangedCallback(OnTextValChanged)));
+
+        private static void OnTextValChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            NewTextBox tBox = (NewTextBox)d;
+            tBox.OnTextValChanged(e);
+
+        }
+
+        private void OnTextValChanged(DependencyPropertyChangedEventArgs e)
+        {
+            textBox.Text = (string)e.NewValue;
+        }
+
+        private static readonly DependencyProperty textWrapping = DependencyProperty.Register(
+            "TextWrapping", typeof(TextWrapping),
+            typeof(NewTextBox),
+            new FrameworkPropertyMetadata(
+                defaultValue: TextWrapping.NoWrap,
+            FrameworkPropertyMetadataOptions.AffectsRender,
+            new PropertyChangedCallback(OnTextWrapChanged)));
+
+        private static void OnTextWrapChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            NewTextBox tBox = (NewTextBox)d;
+            tBox.OnTextWrapChanged(e);
+
+        }
+
+        private void OnTextWrapChanged(DependencyPropertyChangedEventArgs e)
+        {
+            textBox.TextWrapping = (TextWrapping)e.NewValue;
+        }
+
+        public event EventHandler? OnTextChanged;
+        public NewTextBox()
+        {
+            InitializeComponent();
+            textBox.Text = (string)GetValue(text);
+            textBox.TextChanged += textBox_TextChanged;
+        }
+
+        public string Text { get => (string)GetValue(text); set => SetValue(text, value); }
+
+        public TextWrapping TextWrapping
+        {
+            get => (TextWrapping)GetValue(textWrapping); set => SetValue(textWrapping, value);
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTextChanged?.Invoke(sender, e);
+            SetValue(text, textBox.Text);
+        }
+
+        private void Grid_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            textBox.Focus();
+        }
+
+        private void Grid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+
+            textBox.Focus();
+        }
+    }
+}
---
+++ IDMSFormNameGenerator/Pages/HomePage.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IDMSFormNameGenerator.Pages
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : UserControl
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Properties.Resources.GitHub,
+                UseShellExecute = true
+            });
+        }
+
+        private void btnADP_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Properties.Resources.ADP,
+                UseShellExecute = true
+            });
+        }
+
+        private void btnSF_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Properties.Resources.SalesForce,
+                UseShellExecute = true
+            });
+        }
+
+        private void btnMyApps_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Properties.Resources.MyApps,
+                UseShellExecute = true
+            });
+        }
+
+        private void btnClientInfoReport_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Properties.Resources.ClientInfoReport,
+                UseShellExecute = true
+            });
+
+        }
+
+        private void btnFormsTracker_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Properties.Resources.FormsTracker,
+                UseShellExecute = true
+            });
+        }
+
+        private void btnWorkday_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Properties.Resources.Workday,
+                UseShellExecute = true
+            });
+        }
+
+        private void btnCST_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\AmpsSupport\\CSTLoader.exe",
+                UseShellExecute = true
+            });
+        }
+
+        private void btnFormgen_Click(object sender, RoutedEventArgs e)
+        {
+            var Info = new System.Diagnostics.ProcessStartInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\NewFormGen.lnk")
+            {
+                UseShellExecute = true
+            };
+
+            System.Diagnostics.Process.Start(Info);
+
+        }
+    }
+}
